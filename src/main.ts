bb07df147c07c898d5329838e9f78cfe07243f81
Andrew Sun <me@andrewsun.com>
2020-05-16 15:59:50 -0400

    make clone work again

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ export function create(options) {
 import dateReviver from './date-reviver';
 export {dateReviver};
 
-let defaultInstance;
+let defaultInstance: DiffPatcher;
 
 export function diff() {
   if (!defaultInstance) {
@@ -28,23 +28,9 @@ export function patch() {
   return defaultInstance.patch.apply(defaultInstance, arguments);
 }
 
-export function unpatch() {
+export function clone<T>(obj: T): T {
   if (!defaultInstance) {
     defaultInstance = new DiffPatcher();
   }
-  return defaultInstance.unpatch.apply(defaultInstance, arguments);
-}
-
-export function reverse() {
-  if (!defaultInstance) {
-    defaultInstance = new DiffPatcher();
-  }
-  return defaultInstance.reverse.apply(defaultInstance, arguments);
-}
-
-export function clone() {
-  if (!defaultInstance) {
-    defaultInstance = new DiffPatcher();
-  }
-  return defaultInstance.clone.apply(defaultInstance, arguments);
+  return defaultInstance.clone(obj);
 }
